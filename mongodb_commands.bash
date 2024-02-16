636ddce3953a2aa44f5feea202057fe0cf727d9a
alinept <alineporfirot@gmail.com>
2024-02-16 13:28:38 -0500

    command for deleting documents in a collection

--- mongodb_commands.bash
+++ mongodb_commands.bash
@@ -40,4 +40,13 @@ db.series.find({"Ano de lançamento": {$in: [2019,2020]}})
 db.series.find({}, {Série: 1, Classificação: 1, _id: 0}).sort({"Série":1})
 db.series.find({"Temporadas disponíveis": {$gte: 4}})
 db.series.find({"Ano de lançamento": {$ne: 2020}}) #value different of
+db.series.find({"Genero": {$all:["Ação", "Comédia"]}})
+
+db.series.update({}, { $rename: {"Genero": "Gênero" }}, false, true); #modifier field name
+
+db.series.updateOne({"Série": "Grimm"},{$set: {"Temporadas disponíveis": 6}})
+db.series.updateMany({"Série":{$in:["Four More Shots Please", "Fleabag"]}},{$set: {"Classificação": "18+"}})
+
+db.series.deleteOne({"Série": "The Boys"})
+db.series.deleteMany({"Temporadas disponíveis": 1})
 
